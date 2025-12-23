22c19aae8ce921792e1a0cb7adc77d9fd4bb9e19
AnandJadhav08 <anandprofessionalactivity2308@gmail.com>
2025-12-23 19:06:57 +0530

    Refactor code structure for improved readability and maintainability Leetcode Problem : #334

---
+++ TwoSum.cpp
@@ -0,0 +1,62 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void reverseString(vector<char>& s)
+    {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while(left < right) {
+          swap(s[left], s[right]);
+
+           left++;
+           right--;
+        }
+    }
+};
+
+int main()
+{
+    Solution obj;
+
+    vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+
+    cout << "Original String : ";
+    for (char c : s)
+    {
+        cout << c << " ";
+    }
+
+    obj.reverseString(s);
+
+    cout << "\n Reversed String : ";
+    for (char c : s)
+    {
+        cout << c << " ";
+    }
+
+    return 0;
+}
+
+/* Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character. */
