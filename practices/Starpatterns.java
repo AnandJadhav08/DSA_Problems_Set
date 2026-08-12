8445920d69a357eb7bae631fe0887e51f2f17a22
AnandJadhav08 <anandprofessionalactivity2308@gmail.com>
2026-08-12 16:03:00 +0530

    star pattern exercise

---
+++ practices/Starpatterns.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class Starpatterns {
+
+    public static void main(String[] args) {
+
+        System.out.println("zzzzzzzzzzzzzz Star Pyramid - Inverted Triangle zzzzzzzzzzzzz");
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 5; j > i; j--) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("zzzzzzzzzzzzzz Star Pyramid - Right Triangle zzzzzzzzzzzzz");
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("zzzzzzzzzzzzzz Star - Square zzzzzzzzzzzzz");
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j < 5; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("zzzzzzzzzzzzzz Number Triangle zzzzzzzzzzzzz");
+         for (int i = 1 ; i <= 5 ; i++){
+            for (int j = 1 ; j <= i ; j++){
+                System.out.print(j);
+            }
+      System.out.println();
+         }
+
+         System.out.println("zzzzzzzzzzzzzz Double Number Triangle zzzzzzzzzzzzz");
+         for (int i = 1 ; i <= 5 ; i++){
+            for (int j = 1 ; j <= i ; j++){
+                System.out.print(i);
+            }
+      System.out.println();
+         }
+    }
+}
+
+
+
+/* zzzzzzzzzzzzzz Star Pyramid - Inverted Triangle zzzzzzzzzzzzz
+*****
+****
+***
+**
+*
+zzzzzzzzzzzzzz Star Pyramid - Right Triangle zzzzzzzzzzzzz
+*
+**
+***
+****
+*****
+zzzzzzzzzzzzzz Star - Square zzzzzzzzzzzzz
+*****
+*****
+*****
+*****
+*****
+zzzzzzzzzzzzzz Number Triangle zzzzzzzzzzzzz
+1
+12
+123
+1234
+12345
+zzzzzzzzzzzzzz Double Number Triangle zzzzzzzzzzzzz
+1
+22
+333
+4444
+55555 */
