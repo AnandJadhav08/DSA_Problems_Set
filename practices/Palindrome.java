883c0d17e1e36cc30a2757ffdedc6de5376baa8b
AnandJadhav08 <anandprofessionalactivity2308@gmail.com>
2026-08-13 23:58:49 +0530

    palindrome question

---
+++ practices/Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Palindrome {
+    
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter String and Find Given Word is Palindrome or Not !");
+    String str = sc.nextLine();
+    int left = 0;
+    int right = str.length() - 1;
+    boolean isPalindrome = true;
+
+    while (left < right) {
+        if (str.charAt(left) != str.charAt(right)) {
+            isPalindrome = false;
+            break;
+        }
+        left++;
+        right--;
+    }
+      System.out.println("Given String Palindrome is: "+ isPalindrome);  
+    }
+}
